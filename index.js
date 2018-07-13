f8d622460e47c83863b59b0f069a4ddf325007ef
Sean Christopher Febo <scf@techie.com>
2018-07-13 21:26:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your code in this file!
+function scuberGreetingForFeet(someNumber) {
+  if (someNumber <= 400) {
+    return 'This one is on me!';
+  }
+  else if (someNumber > 400 && someNumber <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (someNumber > 2500) {
+    return 'No can do.';
+  }
+}
+
+function ternaryCheckCity(someCity) {
+  let value;
+  value = someCity === 'NYC' ? "Ok, sounds good." : "No go.";
+  return value;
+}
+
+function switchOnCharmFromTip(someExpression) {
+  switch (someExpression) {
+  case 'generous':
+    return "Thank you so much.";
+  case 'not as generous': 
+    return "Thank you.";
+  default:
+    return "Bye.";
+  }
+}
