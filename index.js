e1292f440889da6d0cf101a777e0aea8d306c55b
Sean Christopher Febo <scf@techie.com>
2018-07-13 21:07:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+function scuberGreetingForFeet(someNumber) {
+  if (someNumber <= 400) {
+    return 'This one is on me!';
+  }
+  else if (someNumber > 400 && someNumber <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (someNumber > 2500) {
+    return 'No can do.';
+  }
+}
